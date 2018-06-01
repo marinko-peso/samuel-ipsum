b2b34d8f22b5b365f2ebbd929488467c39c73aa9
Dario Vladovic <d.vladimyr@gmail.com>
2018-06-01 20:29:48 +0200

    Validate quotes database
    - extracting remote url to `package.json` config section

--- scripts/fetch-quotes.js
+++ scripts/fetch-quotes.js
@@ -1,20 +1,22 @@
 'use strict';
 
+const { config } = require('../package.json');
 const r = require('got');
-const quotesUrl = 'http://slipsum.com/js/slipsum.js';
 
 const reIpsum = /^\s*ipsum\[\d+\]\s*=\s*['"](.*?)['"]\s*;\s*$/;
 const reSlip = /^\s*slips\[\d+\]\s*=\s*['"](.*?)['"]\s*;\s*$/;
 
 const exec = (str, regex) => str.match(regex) || [];
+const isEmpty = (arr = []) => arr.length <= 0;
 const jsonify = obj => JSON.stringify(obj, null, 2);
 
-fetchQuotes(quotesUrl)
+fetchQuotes(config.quotesUrl)
+  .then(data => validate(data))
   .then(data => console.log(jsonify(data)))
   .catch(err => { throw err; });
 
 async function fetchQuotes(url) {
-  const { body: code } = await r.get(quotesUrl);
+  const { body: code } = await r.get(url);
   const lines = code.split(/\r?\n/);
   const data = { ipsums: [], slips: [] };
   return lines.reduce((acc, line) => {
@@ -29,4 +31,11 @@ async function fetchQuotes(url) {
     }
     return acc;
   }, data);
+}
+
+function validate(data = {}) {
+  if (isEmpty(data.ipsums) || isEmpty(data.slips)) {
+    throw new Error('Failed to grab `ipsums` and/or `slips` from remote source');
+  }
+  return data;
 }
